52d00d866f1d281438d9bc5393a05d0b85067533
violaine <e.violaine@yahoo.fr>
2022-02-09 18:03:19 +0100

    card.js style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Card = ({ name, capital, population, flag }) => {
 	return (
-		<li className="Card">
+		<li className="card">
 			<img src={flag} alt="flag" />
 			<div className="data-container">
 				<ul>
 					<li>{name}</li>
 					<li>capitale :{capital}</li>
-					<li>population: {population}</li>
+					<li>population : {population}</li>
 				</ul>
 			</div>
 		</li>
